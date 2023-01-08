4bf5fe34dfd40944e10a7cfdf0f6c8e2d12048be
Crisvc <117755400+Crisvc@users.noreply.github.com>
2023-01-08 15:00:27 -0500

    Final Draft

--- src/com/coderscampus/Main.java
+++ src/com/coderscampus/Main.java
@@ -1,60 +1,52 @@
 package com.coderscampus;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		
+
 		UserService userService = new UserService();
-		User [] users = new User[4];
-		BufferedReader fileReader = null;
-		try {
-			fileReader = new BufferedReader(new FileReader("data.txt"));
-			String line;
-		
-			int i=0;
-			while ((line = fileReader.readLine()) != null) {
-				String [] values = line.split(",");
-				String username = values [0];
-				String password = values [1];
-				String name = values [2];
-				
-				users[i] = userService.CreateUser(username, password, name);
-				i++;
-				
-				
-		}
-		
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter Username:");
-		String username = input.next();
-		System.out.println("Please enter Password:");
-		String password = input.next();
-		
-		for (User user : users) {
-			if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-				System.out.println("Welcome " + user.getName());
-				break;
-			}	else {
-				System.out.println("Invalid login. Please Try Again");
-				
-			}
-			{
+		User[] users = userService.getUserArray();
+
+		Scanner scanner = new Scanner(System.in);
+		boolean success = false;
+		int attempt = 0;
+		while (attempt < 3) {
+			System.out.println("Please enter Username:");
+			String username = scanner.next();
+			System.out.println("Please enter Password:");
+			String password = scanner.next();
 			
+			if (userVal (users, username, password) != true) {
+				if (attempt < 2) {
+					System.out.println("Invalid Login, Please try again");					
+				}
+				attempt++; 
+			} else {
+				success = true;
+				break;
 			}
+			
 		}
+		if (success != true) {
+			System.out.println("Too many attempts, you are locked out");
+		}
+		scanner.close();
+	}
+
+	
+
+	private static boolean userVal(User[] users, String username, String password) {
+
+		for (User user : users)
+			if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
+				System.out.println("Welcome " + user.getName());
+				return true;
 			}
+		return false;
 
 	}
 
+}
--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -1,16 +1,42 @@
 package com.coderscampus;
 
-
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class UserService {
 
-	public User CreateUser (String username, String password, String name) {
+	public User createUser (String username, String password, String name) {
 		User user = new User ();
 		user.setUsername(username);
 		user.setPassword(password);
 		user.setName(name);
 		return user;
+	}
+	
+	public User[] getUserArray() {
+		User[] users = new User[4];
+		BufferedReader fileReader = null;
+		try {
+			fileReader = new BufferedReader(new FileReader("data.txt"));
+			String line;
+
+			int i = 0;
+			while ((line = fileReader.readLine()) != null) {
+				String[] values = line.split(",");
+				String username = values[0];
+				String password = values[1];
+				String name = values[2];
+
+				users[i] = createUser(username, password, name);
+				i++;
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return users;
 	}
 
 
